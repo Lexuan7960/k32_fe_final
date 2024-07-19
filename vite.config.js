74a1204b4c5ec3a48d69cbbc60d2716dfb0418ae
Lexuan7960 <153303800+Lexuan7960@users.noreply.github.com>
2024-07-19 21:44:17 +0700

    New update Fronend K32

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,10 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    port: 10000, // Sử dụng cổng 10000
+    host: true,  // Để Vite lắng nghe trên tất cả các địa chỉ IP
+  },
   build: {
   outDir: 'dist',
   },
